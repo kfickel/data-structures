383074803c8a0c124e9529d577034c11ea9d1f6d
Hack Reactor Student <student@hackreactor.com>
2017-10-11 20:40:11 -0700

    Completed prototypal patten queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,38 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+
+  window.storage = {};
+  window.count = 1;  
+
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = value => {
+  if (count in storage) {
+    count++;
+  }
+  storage[count] = value;
+};
+
+queueMethods.dequeue = () => {
+  var keys = Object.keys(storage);
+  var values = Object.values(storage);
+
+  if (keys.length) {
+    delete storage[keys[0]];
+  }
+  if (values.length) {
+    return values[0];
+  }
+};
+
+queueMethods.size = () => {
+  var keys = Object.keys(storage);
+  
+  return keys.length;
+};
 
