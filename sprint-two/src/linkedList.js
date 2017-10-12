b242ed1b5abde99ee2334965b3e3de359b819051
Hack Reactor Student <student@hackreactor.com>
2017-10-12 16:18:10 -0700

    linkedList without .contains filled in

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    list.tail = value;
+    var newNode = new Node(value);
+    var checkNode = list.head;
+    
+    if (!checkNode) {
+      list.head = newNode;
+      return newNode;
+    } 
+
+    while (checkNode.next) {
+      checkNode = checkNode.next;
+    }
+
+    checkNode.next = newNode;
+    return newNode;
   };
 
   list.removeHead = function() {
+    //remove the value of list.head
+    //set list.head to the next node
+    var values = Object.values(list);
+
+    if (list.head) {
+      list.head = list.head.next;
+    }
+
+    return values[0];
   };
 
   list.contains = function(target) {
+    //returns true or false if target in node
+    
   };
 
   return list;
