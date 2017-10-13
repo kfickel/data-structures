44bf26bceb387b6a4ca1292a6f70902d9d7142e4
Hack Reactor Student <student@hackreactor.com>
2017-10-12 21:39:20 -0700

    Completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,38 +4,53 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    list.tail = value;
+    //create a new node
     var newNode = new Node(value);
+    //create variable to check current node
     var checkNode = list.head;
+    //designate the tail to a new node
+    list.tail = newNode;
     
+    //check if the current node (list.head specifically) does not exists
     if (!checkNode) {
+      //create a head node
       list.head = newNode;
       return newNode;
     } 
-
+    //while check current node key next exists
     while (checkNode.next) {
+      //set current node to the next node's next
       checkNode = checkNode.next;
     }
-
+    
+    //nesting the new node in the old node's next (which was previously null aka the tail)
     checkNode.next = newNode;
     return newNode;
   };
 
   list.removeHead = function() {
-    //remove the value of list.head
-    //set list.head to the next node
-    var values = Object.values(list);
+    var value = list.head.value;
 
     if (list.head) {
+      //"remove current head" by setting list.head to the next node
       list.head = list.head.next;
     }
 
-    return values[0];
+    return value;
   };
 
   list.contains = function(target) {
-    //returns true or false if target in node
-    
+    //returns true if target in list. False if not
+    var currentNode = list.head;
+    //as long as currentNode.next exists
+    while (currentNode.next) {
+      //Check current node value equals the target value
+      if (currentNode.value === target) { return true; }
+      //if not, increment current node to the next node
+      currentNode = currentNode.next;
+    }
+    //check last value. return true or false
+    return currentNode.value === target;
   };
 
   return list;
