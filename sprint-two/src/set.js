3d6f56f256d141956f27ef78a76a37a5ca44f4f6
Hack Reactor Student <student@hackreactor.com>
2017-10-13 20:35:57 -0700

    Completed set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //if obj does not have property item
+  if (!this.storage.hasOwnProperty(item)) {
+    //add value item to obj
+    this.storage[item] = item;
+  }
+  
+  
 };
 
 setPrototype.contains = function(item) {
+  //if obj has property item
+  if (this.storage.hasOwnProperty(item)) { return true; }
+  //return false
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  //if obj has property item
+  if (this.storage.hasOwnProperty(item)) {
+    //delete obj item
+    delete this.storage[item];
+  }
 };
 
 /*
